5d6baa41be12346f7fba961a58794e92e22e6f1c
Simon Sankar <simon.sankar@wepala.com>
2020-06-17 15:32:10 -0400

    Tweaked imports on AgendaHOC

--- src/tasks/controllers/AgendaHOC.js
+++ src/tasks/controllers/AgendaHOC.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import {Controller} from '../../controller';
-import {getTasksByDate} from '../model/selectors';
 import moment from 'moment';
 import {startTask, updateTask} from '../model/commands';
 import {addTimeLog} from '../../logs/model/commands';
+import {getTasksByDate} from '../model/selectors';
 import {getTaskTimeSpentByDate} from '../../logs/model/selectors';
 
 const AgendaHOC = (WrappedComponent, props) => {
