bf70bc945253196bbed46e66dee73a5cd2b5a594
Simon Sankar <simon.sankar@wepala.com>
2020-06-30 15:10:24 -0400

    AGENDA-213 Updated AppDelegate and Info.plist

--- ios/WeAgenda/AppDelegate.m
+++ ios/WeAgenda/AppDelegate.m
@@ -66,9 +66,7 @@ static void InitializeFlipper(UIApplication *application) {
 - (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
  restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
 {
- return [RCTLinkingManager application:application
-                  continueUserActivity:userActivity
-                    restorationHandler:restorationHandler];
+ return [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
 }
 
 @end
